1fa3c0e2bbae9a5e8e9f4b16c015d90ca5b8db1b
Michal Urban <mcraftpl2@gmail.com>
2023-02-06 20:13:51 +0100

    custom user inherited from abstract user with migrations

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-##azeno
+## Azeno Flashcards
+''
--- bakend_core/config/settings.py
+++ bakend_core/config/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 
-    'users'
+    'users.apps.UsersConfig',
+    # 'users',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = 'users.CustomUser'
